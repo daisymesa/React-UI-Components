37cfc9997774a08a0d3497eabcbfac2c575e7755
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-22 18:47:06 -0400

    Added card banner component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 
+import CardContainer from './components/CardComponents/CardContainer';
+
 const App = () => {
   return (
     <div className = "app-container">
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
