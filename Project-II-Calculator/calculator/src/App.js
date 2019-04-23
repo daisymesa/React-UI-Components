2503082253c831b5bc7a8d29745be68ff988068d
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-23 18:11:29 -0400

    Added number buttons for calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,73 @@
 import React from 'react';
 import './App.css';
 
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+
+
+// const App = () => {
+//   return (
+//     <div className = "app-container">
+//     <NumberButton />
+//     <ActionButton />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    // <div className="action-button-row">
+    //   <button className="action-button"> clear </button>
+    //   <button className="operator-button"> / </button>
+    // </div>
+
+    <div className="button-rows-section">
+      <div className="buttons-row">
+        <button> 7 </button>
+        <button> 8 </button>
+        <button> 9 </button>
+        <button className="operator-button"> x </button>
+      </div>
+
+      <div className="buttons-row">
+        <button> 4 </button>
+        <button> 5 </button>
+        <button> 6 </button>
+        <button className="operator-button"> - </button>
+      </div>
+
+      <div className="buttons-row">
+        <button> 1 </button>
+        <button> 2 </button>
+        <button> 3 </button>
+        <button className="operator-button"> + </button>
+      </div>
     </div>
+
   );
 };
 
 export default App;
+
+
+
+
+
+
+// <div>
+// <h3>Welcome to React Calculator</h3>
+// <p>
+//   We have given you a starter project. You'll want to build out your
+//   components in their respective files, remove this code and replace it
+//   with the proper components.
+// </p>
+// <p>
+//   <strong>
+//     Don't forget to `default export` your components and import them here
+//     inside of this file in order to make them work.
+//   </strong>
+// </p>
+// </div>
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <div className = "number-button">
+
+        </div>
+    );
+};
+
+
+// const numButtons = [ {number: 1}, {number: 2}, {number: 3}, {number: 4}, {number: 5}, {number: 6},{number: 7}, {number: 8}, {number: 9} ];
+
+
+// function Buttons () {
+//     return (
+//         <div>
+//             {numButtons.map(numbersFromMap => {
+//                 return <NumberButton numberProp = {numbersFromMap} />
+//             })}
+//         </div>
+//     )
+// }
+
+// console.log(Buttons());
+
+export default NumberButton;
